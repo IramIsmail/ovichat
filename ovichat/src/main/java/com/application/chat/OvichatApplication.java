9e6413fb08fb1e22fa23f158218eda61e4051bd9
Iram Ismail <iram.8859@gmail.com>
2020-12-07 15:49:48 +0530

    Initial Comit

---
+++ ovichat/src/main/java/com/application/chat/OvichatApplication.java
@@ -0,0 +1,13 @@
+package com.application.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvichatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvichatApplication.class, args);
+	}
+
+}
---
+++ ovichat/src/test/java/com/application/chat/OvichatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvichatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
